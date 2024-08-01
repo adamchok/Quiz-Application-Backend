40706f36edf2ae9b83f8d0cc78252d267ff0a15c
adamchok <adam.c11304@gmail.com>
2024-08-01 17:38:43 +0800

    Create QuestionController.java

---
+++ src/main/java/com/project/quiz/controller/QuestionController.java
@@ -0,0 +1,73 @@
+package com.project.quiz.controller;
+
+import com.project.quiz.model.Question;
+import com.project.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.DeleteExchange;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/quiz")
+@CrossOrigin("*")
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> questions = questionService.getAllQuestions();
+        return ResponseEntity.ok(questions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Question> getQuestionById(@PathVariable Long id) {
+        Optional<Question> questionOpt = questionService.getQuestionById(id);
+        return questionOpt
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
+        try {
+            Question savedQuestion = questionService.createQuestion(question);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedQuestion);
+        } catch (Exception e) {
+            e.printStackTrace(); // It's better to log the exception
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/difficulty/{difficulty}")
+    public ResponseEntity<List<Question>> getQuestionsByDifficulty(@PathVariable String difficulty) {
+        List<Question> questions = questionService.getQuestionsByDifficulty(difficulty);
+        if (questions.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.ok(questions);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteQuestionById(@PathVariable Long id) {
+        boolean isSuccess = questionService.deleteQuestionById(id);
+
+        if (isSuccess) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body("Question deleted successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question not found.");
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Question> updateQuestionById(@PathVariable Long id, @RequestBody Question question) {
+        return questionService.updateQuestionById(id, question)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+}
