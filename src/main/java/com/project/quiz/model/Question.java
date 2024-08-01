f1c608fa33b4be2a62246a3404cb9324953ff76b
adamchok <adam.c11304@gmail.com>
2024-08-01 17:38:30 +0800

    Created questions model

---
+++ src/main/java/com/project/quiz/model/Question.java
@@ -0,0 +1,41 @@
+package com.project.quiz.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "questions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "option_1")
+    private String option_1;
+
+    @Column(name = "option_2")
+    private String option_2;
+
+    @Column(name = "option_3")
+    private String option_3;
+
+    @Column(name = "option_4")
+    private String option_4;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @Column(name = "difficulty_level")
+    private String difficulty_level;
+
+}
